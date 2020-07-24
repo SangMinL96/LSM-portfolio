b6062546d2ca8a4adc0508cce5930ea7edae9629
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-07-25 02:38:01 +0900

    SKILL 섹션 HTML,CSS,script 구성
    아이템 배열넘버를 랜덤으로 받음
    left,top 값을 랜덤으로 받아 style 적용하여 자동으로 움직이는 모션만듬.

---
+++ js/skill.js
@@ -0,0 +1,22 @@
+const items = document.querySelectorAll(".item_name");
+
+const itemNumber = () => {
+  const Num = Math.floor(Math.random() * items.length);
+  return Num;
+};
+
+const itemWidth = () => {
+  const x = Math.floor(Math.random() * (350 - 50)) + 50;
+  return x;
+};
+const itemHeight = () => {
+  const y = Math.floor(Math.random() * (380 - 50)) + 50;
+  return y;
+};
+
+function onMove() {
+  const itemRandom = itemNumber();
+  items[itemRandom].style.left = itemWidth() + "px";
+  items[itemRandom].style.top = itemHeight() + "px";
+}
+setInterval(onMove, 80);
