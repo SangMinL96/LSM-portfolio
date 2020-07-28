16167faa12b4d50a8bc2f9933192391480c6eca4
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-07-29 05:07:17 +0900

    proJact Inside slide 추가

--- js/proJact.js
+++ js/proJact.js
@@ -1,4 +1,4 @@
-// const docValue = document.documentElement;  header.js에서 선언함
+//const docValue = document.documentElement;  header.js에서 선언함
 const leftMove = document.querySelectorAll(".leftMoveItem");
 const centerMove = document.querySelectorAll(".centerMoveItem");
 const rightMove = document.querySelectorAll(".rightMoveItem");
@@ -21,3 +21,40 @@ window.addEventListener("scroll", () => {
     rightMove[2].classList.add("right");
   }
 });
+//
+//
+// 인사이드 클릭 이벤트
+const proJactItems = document.querySelectorAll(".hoverbackcolor");
+const inItems = document.getElementsByClassName("item_inside");
+const closeIcon = document.querySelectorAll(".item_close");
+
+const slideMove = (value) => {
+  inItems[value - 1].style.top = "10%";
+  inItems[value - 1].style.opacity = "1";
+  inItems[value - 1].style.zIndex = "99";
+};
+
+const inside = () => {
+  closeIcon.forEach((close) => {
+    close.addEventListener("click", (ev) => {
+      const closeValue = ev.currentTarget;
+      console.dir(closeValue);
+      closeValue.offsetParent.style.top = "-72%";
+      closeValue.offsetParent.style.opacity = "0";
+      closeValue.offsetParent.style.zIndex = "-1";
+    });
+  });
+};
+
+const onSlide = () => {
+  proJactItems.forEach((item) => {
+    item.addEventListener("click", (ev) => {
+      const itemValue = ev.target.id;
+      console.log(itemValue);
+      slideMove(itemValue);
+    });
+  });
+};
+
+onSlide();
+inside();
